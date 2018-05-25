4a801e395b926e7a92a3a0cff3c70dbc395f0c25
Justin Dane D. Vallar <jdvallar@gmail.com>
2018-05-25 14:37:20 +0800

    Change development environment to custom react-scripts

--- src/docs/Docs.js
+++ src/docs/Docs.js
@@ -1,13 +1,12 @@
 import React from 'react';
 // eslint-disable-next-line
 import ComponentPage from './ComponentPage';
-import componentData from '../../config/componentData';
+import componentData from '../../componentData';
 
 import Layout from 'doj-react-adminlte/Layout';
 import SidebarTree from "doj-react-adminlte/Layout/SidebarTree";
 import SidebarItem from "doj-react-adminlte/Layout/SidebarItem";
 import SidebarHeader from "doj-react-adminlte/Layout/SidebarHeader";
-import UserMenu from "doj-react-adminlte/Layout/UserMenu";
 
 export default class Docs extends React.Component {
     constructor(props) {
