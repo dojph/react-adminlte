a2baf12a789e7f39e11517e73a65cafc9485b4e2
Justin Dane D. Vallar <jdvallar@gmail.com>
2018-06-05 15:42:59 +0800

    Implement named imports under Form subdirectory for form components

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,1 +1,9 @@
 export {default} from './Form';
+export {default as CalendarInput} from './CalendarInput';
+export {default as CheckBox} from './CheckBox';
+export {default as FileInput} from './FileInput';
+export {default as Radio} from './Radio';
+export {default as RadioGroup} from './RadioGroup';
+export {default as SelectInput} from './SelectInput';
+export {default as TextArea} from './TextArea';
+export {default as TextInput} from './TextInput';
