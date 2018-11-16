5871f3d7dd0a5fac823d85e7d2ae79d33e2d9d1b
Justin Dane D. Vallar <jdvallar@gmail.com>
2018-11-16 10:20:51 +0800

    Make alert dismissable through show prop

--- src/docs/components/Alert/examples/02-ExampleAlert.js
+++ src/docs/components/Alert/examples/02-ExampleAlert.js
@@ -19,24 +19,23 @@ export default class ExampleAlert extends React.Component{
 
     render() {
         const {show}= this.state;
-        if (show) {
-            return (
-                <div>
-                    <Alert iconClass='fa fa-info' type='alert-danger' onClose={this.handleDismiss}>
-                        <h1>Warning!!</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        </p>
-                        <p>
-                            <button className='btn btn-success' onClick={this.handleDismiss}>Hide Alert</button>
-                        </p>
-                    </Alert>
-                </div>
-            );
-        }
 
-        return <button className='btn btn-danger' onClick={this.handleShow}>Show Alert</button>;
+        return (
+            <div>
+                <Alert iconClass='fa fa-info' type='alert-danger' onClose={this.handleDismiss} show={show}>
+                    <h1>Warning!!</h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                    <p>
+                        <button className='btn btn-success' onClick={this.handleDismiss}>Hide Alert</button>
+                    </p>
+                </Alert>
+
+                {!show && <button className='btn btn-danger' onClick={this.handleShow}>Show Alert</button>}
+            </div>
+        );
     }
 }
