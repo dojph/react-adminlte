d1bd0874a079625da73141fed054400911675c01
Justin Dane D. Vallar <jdvallar@gmail.com>
2019-01-03 14:41:54 +0800

    Add copy button in Example component

--- src/docs/CodeExample.js
+++ src/docs/CodeExample.js
@@ -11,12 +11,46 @@ const customStyle = {
     margin: 0
 };
 
+const buttonStyle = {
+    position: "absolute",
+    top: "10px",
+    right: "10px"
+};
+
 class CodeExample extends React.Component {
+    // Taken from: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+    copyToClipboard = () => {
+        const str = this.props.children;
+
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        const selected =
+            document.getSelection().rangeCount > 0
+                ? document.getSelection().getRangeAt(0)
+                : false;
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        if (selected) {
+            document.getSelection().removeAllRanges();
+            document.getSelection().addRange(selected);
+        }
+    };
+
     render() {
         return (
-            <SyntaxHighlighter language='jsx' style={prism} customStyle={customStyle}>
-                {this.props.children}
-            </SyntaxHighlighter>
+            <div style={{position: "relative"}}>
+                <button className="btn btn-default btn-sm" style={buttonStyle} onClick={this.copyToClipboard}>
+                    Copy
+                </button>
+                <SyntaxHighlighter language='jsx' style={prism} customStyle={customStyle}>
+                    {this.props.children}
+                </SyntaxHighlighter>
+            </div>
         );
     }
 }
