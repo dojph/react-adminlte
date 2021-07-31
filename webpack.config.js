4a826fe391d8760bbe995247d12c12f513ba9bac
Justin Dane D. Vallar <jdvallar@gmail.com>
2021-08-01 05:33:20 +0800

    Remove webpack.config.js from ignored files

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        alias: {
+            'doj-react-adminlte': path.resolve(__dirname, 'src/components')
+        },
+    }
+};
